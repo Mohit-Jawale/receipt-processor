263aa7ffee83cb0519a5fcc82eb10c2bf8318f7b
Mohit-Jawale <61145739+Mohit-Jawale@users.noreply.github.com>
2025-02-05 13:21:47 -0800

    adding receipt model for the post request refactoring

--- internal/handlers/receipt.go
+++ internal/handlers/receipt.go
@@ -8,6 +8,17 @@ import (
 
 func ProcessReceipt(ctx *gin.Context) {
 
+	var receipt models.Receipt
+
 	ctx.JSON(http.StatusOK, gin.H{"message": "Receipt processed successfully"})
+
+	if err := ctx.ShouldBindJSON(&receipt); err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": "Invalid receipt format"})
+		return
+	}
+
+	receiptID := services.GenerateReceiptID()
+
+	ctx.JSON(http.StatusOK, gin.H{"id": receiptID})
 
 }
---
+++ internal/models/receipt.go
@@ -0,0 +1,16 @@
+package models
+
+// Receipt represents a store receipt
+type Receipt struct {
+	Retailer     string `json:"retailer"`
+	PurchaseDate string `json:"purchaseDate"`
+	PurchaseTime string `json:"purchaseTime"`
+	Items        []Item `json:"items"`
+	Total        string `json:"total"`
+}
+
+// Item represents an item on the receipt
+type Item struct {
+	ShortDescription string `json:"shortDescription"`
+	Price            string `json:"price"`
+}
